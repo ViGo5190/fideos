42c930ca4443e6038018f382c42763e09dbe6e21
Stan Gumeniuk <i@vigo.su>
2015-01-08 01:26:54 +0800

    [add] response header

--- src/framework/response.php
+++ src/framework/response.php
@@ -5,15 +5,33 @@
 
 
 $_ENV['_response_'] = array(
-    'content' => ''
+    'content' => '',
+    'headers' => array(),
 );
 
 function framework_response_setContent($content = ''){
     $_ENV['_response_']['content'] = $content;
 }
 
+function framework_response_addHeaders($header = ''){
+    $_ENV['_response_']['headers'][] = $header;
+}
+
 
 function framework_response_send()
 {
+    if (count($_ENV['_response_']['headers'])>0)
+    {
+        foreach ( $_ENV['_response_']['headers'] as $header){
+            header($header);
+        }
+    }
     echo $_ENV['_response_']['content'];
+}
+
+
+
+function framework_response_helper_setContentTypeJson()
+{
+    framework_response_addHeaders('Content-Type: application/json');
 }
